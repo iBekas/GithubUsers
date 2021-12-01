d5cebcc4dad76fd2107ada7493870d1d51602175
iBekas <dmitrbekasov@gmail.com>
2021-12-01 22:54:30 +0300

    Переход на адекватную версию взаимодействия адаптера с фрагментом.

--- app/src/main/java/ru/fylmr/poplibs_nov21/ui/users/UsersPresenter.kt
+++ app/src/main/java/ru/fylmr/poplibs_nov21/ui/users/UsersPresenter.kt
@@ -5,14 +5,11 @@ import moxy.MvpPresenter
 import ru.fylmr.poplibs_nov21.domain.GithubUsersRepository
 import ru.fylmr.poplibs_nov21.model.GithubUserModel
 import ru.fylmr.poplibs_nov21.screens.AppScreens
-import ru.fylmr.poplibs_nov21.ui.base.IListPresenter
 
 class UsersPresenter(
     private val router: Router,
     private val usersRepository: GithubUsersRepository,
 ) : MvpPresenter<UsersView>() {
-
-    val usersListPresenter = UsersListPresenter(router)
 
     override fun onFirstViewAttach() {
         super.onFirstViewAttach()
@@ -22,28 +19,16 @@ class UsersPresenter(
 
     private fun loadData() {
         val users = usersRepository.getUsers()
-        usersListPresenter.users.addAll(users)
 
-        viewState.updateList()
+        viewState.updateList(users)
+    }
+
+    fun onUserClicked(userModel: GithubUserModel) {
+        router.navigateTo(AppScreens.detailScreen(userModel.login))
     }
 
     fun backPressed(): Boolean {
         router.exit()
         return true
-    }
-
-    class UsersListPresenter(private val router: Router) : IListPresenter<UserItemView> {
-
-        val users = mutableListOf<GithubUserModel>()
-
-        override var itemClickListener: (UserItemView) -> Unit =
-            { router.navigateTo(AppScreens.detailScreen(users[it.pos].login)) }
-
-        override fun getCount() = users.size
-
-        override fun bindView(view: UserItemView) {
-            val user = users[view.pos]
-            view.setLogin(user.login)
-        }
     }
 }
